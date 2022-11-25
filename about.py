7444abc32d179f088f59906636f57dab9e00a156
YZZ <myzzzyearz@outlook.com>
2022-11-25 11:57:35 +0800

    update README.md

--- about.py
+++ about.py
@@ -1,6 +1,6 @@
 from Forms.about_dialog import Ui_aboutDialog
 from PySide6.QtCore import Slot
-from PySide6.QtGui import Qt
+from PySide6.QtGui import Qt, QShowEvent, QTextDocument
 from PySide6.QtWidgets import QWidget
 
 
@@ -10,8 +10,20 @@ class AboutDialog(QWidget, Ui_aboutDialog):
         if parent is not None:
             self.setAttribute(Qt.WA_ShowModal, True)
         self.setupUi(self)
-        with open("README.md", "r", encoding="utf-8") as f:
-            self.textBrowser.setMarkdown(f.read())
+
+        with open("./assets/styles/markdown.css", "r") as sf:
+                self.textBrowser.document().setDefaultStyleSheet(sf.read())
+
+    def showEvent(self, event: QShowEvent) -> None:
+        try:
+            with open("README.md", "r", encoding="utf-8") as f:
+                self.textBrowser.setMarkdown(
+                    f.read())
+                self.textBrowser.setHtml(self.textBrowser.toHtml())
+
+        except:
+            self.textBrowser.setHtml("<h1>can't find the file named README.md</h1>")
+        return super().showEvent(event)
 
     @Slot()
     def on_closeBtn_clicked(self):
